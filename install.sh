43cd08dafd598da9f9f138908ca68faebf2e9001
nomuken <nomuken@spicadoll.com>
2015-06-07 03:20:12 +0900

    fixed

--- install.sh
+++ install.sh
@@ -4,26 +4,24 @@ echo "Install NeoBundle"
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 echo "link to my vimrc"
-ln -s `pwd`/dotfiles/vimrc ~/.vimrc
+ln -s `pwd`/vimrc ~/.vimrc
 
 echo "link to my Xmodmap"
-ln -s `pwd`/dotfiles/Xmodmap ~/.Xmodmap
+ln -s `pwd`/Xmodmap ~/.Xmodmap
 
 echo "make xmonad dir"
 mkdir ~/.xmonad
 
 echo "link to xmonad"
-ln -s `pwd`/dotfiles/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s `pwd`/dotfiles/xmobarrc.hs ~/.xmobarrc
+ln -s `pwd`/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s `pwd`/xmobarrc.hs ~/.xmobarrc
 
 echo "link to latexmk"
-ln -s `pwd`/dotfiles/latexmkrc ~/.latexmkrc
+ln -s `pwd`/latexmkrc ~/.latexmkrc
 
 echo "Install Prezto"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 
-for f in ~/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$f" "~/.${f:t}"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-
-
