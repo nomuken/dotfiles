68ac23073e67c3dd2b843b5ba9f9de4f537c3395
Nomuken <nomuken@spicadoll.com>
2015-11-23 22:02:55 +0900

    install.shがナウくなった

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
+DIR=`pwd`
+
 echo "Install NeoBundle"
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 echo "link to my vimrc"
-ln -s `pwd`/vimrc ~/.vimrc
+ln -s $DIR/vimrc ~/.vimrc
 
 echo "link to my Xmodmap"
-ln -s `pwd`/Xmodmap ~/.Xmodmap
+ln -s $DIR/Xmodmap ~/.Xmodmap
 
 echo "make xmonad dir"
 mkdir ~/.xmonad
@@ -16,12 +18,28 @@ echo "link to xmonad"
 ln -s `pwd`/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s `pwd`/xmobarrc.hs ~/.xmobarrc
 
+echo "link to qtile confs"
+ln -s $DIR/config/qtile ~/.config/
+
+echo "link to nvim confs"
+ln -s $DIR/config/nvim ~/.config/
+
+echo "link to nvim confs"
+ln -s $DIR/config/termite ~/.config/
+
 echo "link to latexmk"
-ln -s `pwd`/latexmkrc ~/.latexmkrc
+ln -s $DIR/latexmkrc ~/.latexmkrc
+
+echo "install my ssh key from github"
+curl http://sandbox.spica.bz/sh/installSshKey.sh | bash -
 
 echo "Install Prezto"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+rm -rf .zsh*
+
+for rcfile in "~/.zprezto/runcoms/^README.md(.N)"; do
+  ln -s "$rcfile" "~/.${rcfile:t}"
 done
+
+echo "source $PWD/zshrc" >> ~/.zshrc
