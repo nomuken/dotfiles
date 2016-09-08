6ee34b0578d58fd264c695d1aa68ff5ca75ebfea
Nomuken <nomuken@spicadoll.com>
2016-09-08 13:52:15 +0900

    世界を変えた

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,26 +1,36 @@
-function fish_prompt
-	if not set -q VIRTUAL_ENV_DISABLE_PROMPT
-      set -g VIRTUAL_ENV_DISABLE_PROMPT true
-   end
-   set_color yellow
-   printf '%s' (whoami)
-   set_color normal
-   printf ' at '
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
 
-   set_color magenta
-   printf '%s' (hostname|cut -d . -f 1)
-   set_color normal
-   printf ' in '
+  # User
+  set_color $fish_color_user
+  echo -n (whoami)
+  set_color normal
 
-   set_color $fish_color_cwd
-   printf '%s' (prompt_pwd)
-   set_color normal
+  echo -n '@'
 
-   # Line 2
-   echo
-   if test $VIRTUAL_ENV
-       printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-   end
-   printf '↪ '
-   set_color normal
+  # Host
+  set_color $fish_color_host
+  echo -n (hostname -s)
+  set_color normal
+
+  echo -n ':'
+
+  # PWD
+  if test $VIRTUAL_ENV
+    printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+  end
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  __terlar_git_prompt
+  __fish_hg_prompt
+  echo
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  echo -n '➤ '
+  set_color normal
 end
