72de0c7c542d2686f5409f99df0f1c1ce1c5438d
Nomuken <nomuken@spica.bz>
2025-04-21 01:33:19 +0900

    ask password

--- scripts/ansible.macos.sh
+++ scripts/ansible.macos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export GIT_ROOT_PATH=$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
 
-ansible-playbook -i $GIT_ROOT_PATH/localhost.yaml $GIT_ROOT_PATH/macos.yaml
+ansible-playbook -i $GIT_ROOT_PATH/localhost.yaml $GIT_ROOT_PATH/macos.yaml --ask-become-pass
+
