f3c96d51962697526772a66b699e3e3f86c5ebe1
Nomuken <nomuken@spica.bz>
2025-04-21 01:34:52 +0900

    やっぱ聞かないで

--- scripts/ansible.macos.sh
+++ scripts/ansible.macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export GIT_ROOT_PATH=$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
 
-ansible-playbook -i $GIT_ROOT_PATH/localhost.yaml $GIT_ROOT_PATH/macos.yaml --ask-become-pass
+ansible-playbook -i $GIT_ROOT_PATH/localhost.yaml $GIT_ROOT_PATH/macos.yaml
 
